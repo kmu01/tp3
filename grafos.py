7408d7228021491eb0e2863d9e9182d440e9db3b
Teo-C <71954209+Teo-C@users.noreply.github.com>
2021-03-10 21:36:57 -0300

    Add files via upload

--- grafos.py
+++ grafos.py
@@ -1,4 +1,5 @@
-from random import random.choice
+from random import *
+
 
 # Definimos un Grafo a partir de un diccionario de diccionarios
 class Grafo(object):
@@ -22,6 +23,10 @@ class Grafo(object):
     def obtener_vertices(self):
         """Devuelve lista con los vertices del grafo"""
         return list(self.vertices.keys())
+    
+    def obtener_cantidad_vertices(self):
+        """Devuelve la cantidad de vertices del grafo"""
+        return len(self.vertices.keys())
 
     def pertenece(self,v):
         """Comprueba si el vertice esta dentro del grafo"""
@@ -47,10 +52,17 @@ class Grafo(object):
         return w in self.vertices[v]
 
     def adyacentes(self,v):
+        """Devuelve una lista con los vertices adyacentes a v"""
         return list(self.vertices[v].keys())
 
     def crear_iterador(self):
+        """Crea un iterados"""
         return iter(self.vertices)
     
     def obtener_vertice_aleatorio (self):
-        return random.choice (list(self.vertices.keys()))
+        """Devuelve un vertice de forma aleatoria"""
+        return random.choice(list(self.vertices.keys()))
+    
+    def obtener_adyacente_aleatorio (self,v):
+        """Devuelve un vertice adyacente a v de forma aleatoria"""
+        return random.choice(list(self.vertices[v].keys()))
